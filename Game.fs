1bffcad12b71608fd8c0c271e8dacb15aef4efd2
jhkroger <jhkroger2@gmail.com>
2020-11-22 14:40:37 +0200

    Drawing game board, first steps

--- Game.fs
+++ Game.fs
@@ -5,6 +5,7 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
 open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
 open fsharptesting.Globals
 open fsharptesting.Logic
 open fsharptesting.DrawUtils
@@ -27,10 +28,11 @@ type Game1() as game =
     
     // Game Assets
     let ms_texture = lazy ( game.Content.Load<Texture2D> "minesweeper/mw_assets")
+    let font = lazy ( game.Content.Load<SpriteFont> "myFont")
     let ts = 18.0f
     let grid_width = 19
     let grid_height = 14
-      
+    let mutable (grid_pos_string : string) = sprintf "Pos: (%d, %d)" 1 1
     let null_rect = System.Nullable<Rectangle>()
     
     let draw_number pos (num : int) =
@@ -47,19 +49,29 @@ type Game1() as game =
     let draw_qmark pos = draw_from_sheet(spriteBatch, ms_texture.Value, pos, qmark_rect)
         
     let draw_board (sb : SpriteBatch) =
-        let ts = 18.0f
-        draw_tile Vector2.Zero
-        draw_hl_tile(Vector2(ts, 0.0f))
-        draw_mine (Vector2(ts*2.f, 0.0f))
-        draw_flag (Vector2(ts*3f, 0.0f))
-        draw_qmark (Vector2(ts * 4f, 0f))
+        // Game draw ops -> examples
+        //draw_tile Vector2.Zero
+        //draw_hl_tile(Vector2(ts, 0.0f))
+        //draw_mine (Vector2(ts*2.f, 0.0f))
+        //draw_flag (Vector2(ts*3f, 0.0f))
+        //draw_qmark (Vector2(ts * 4f, 0f))
+        for x in 0.f .. float32 grid_width do
+            for y in 0.f .. float32 grid_height do
+                let pos = Vector2(x*ts, y*ts)
+                draw_tile pos
+        ()
+        
+    
+    
     
     /// Member bindings & overriders below
     /// 
     override game.Initialize() =
         do base.Initialize()
-        graphics.PreferredBackBufferWidth <- screen_width
-        graphics.PreferredBackBufferHeight <- screen_height
+        let preferred_width = grid_width * tile_size + tile_size
+        let preferred_height = grid_height * tile_size + tile_size
+        graphics.PreferredBackBufferWidth <- preferred_width
+        graphics.PreferredBackBufferHeight <- preferred_height
         graphics.IsFullScreen <- false
         game.Window.Title <- "Minesweeper"
         graphics.ApplyChanges()
@@ -70,11 +82,12 @@ type Game1() as game =
         white_texture <- new Texture2D(device, 1, 1)
         white_texture.SetData<Color>([| Color.White |])
         spriteBatch <- new SpriteBatch(device)
-
-        
-        ()
-
+    
+    
     override game.Update(gameTime) =
+        
+        let (m_x, m_y) = mouse_state tile_size grid_width grid_height
+        grid_pos_string <- sprintf "Pos: (%d, %d)" m_x m_y
         ()
 
     override game.Draw(gameTime) =
@@ -83,11 +96,13 @@ type Game1() as game =
         // Start of draw calls from the actual game
         
         draw_board spriteBatch
-
-        let start_pos = Vector2(0.f, ts)
-        for i in 1 .. 8 do
-            let pos = start_pos + Vector2(float32(i - 1)*ts, 0f)
-            do draw_number pos i
+        let (m_x, m_y) = mouse_state tile_size grid_width grid_height
+        let mouse_pos = Vector2(float32(m_x)*ts, float32(m_y)*ts)
+        draw_hl_tile mouse_pos
+        
+        // Draw text & other UI
+        //spriteBatch.DrawString(font, "Score", new Vector2(100, 100), Color.Black);
+        do spriteBatch.DrawString(font.Value, grid_pos_string, Vector2.Zero, Color.Black)
             
         
         // Sprite batch ends, drawing stops
--- MineSweeperUtils.fs
+++ MineSweeperUtils.fs
@@ -1,9 +1,11 @@
 ﻿module fsharptesting.MineSweeperUtils
 
     open Microsoft.Xna.Framework
+    open Microsoft.Xna.Framework.Input
 
     // tile size
-    let tile_size = 19
+    let tile_size = 18
+    let ts = 18.0f
     
     let basic_tile_rect = Rectangle(0,0,tile_size,tile_size)
     let hl_tile_rect = Rectangle(tile_size,0,tile_size,tile_size)
@@ -12,3 +14,13 @@
     let mine_rect = Rectangle(tile_size*3, 0, tile_size, tile_size)
     let flag_rect = Rectangle(tile_size*4, 0, tile_size, tile_size)
     let qmark_rect = Rectangle(tile_size*5, 0, tile_size, tile_size)
+    
+    
+    // functions
+    let mouse_state tile_size max_x max_y =
+        let state = Mouse.GetState()
+        let selected_x = MathHelper.Clamp(state.X / tile_size, 0, max_x)
+        let selected_y = MathHelper.Clamp(state.Y / tile_size, 0, max_y)
+        (selected_x, selected_y)
+        
+        
